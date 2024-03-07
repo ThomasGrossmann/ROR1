d878e18927b13e80f2a61f138d9d748999aecf0e
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-03-07 08:09:16 +0100

    Add basic routes and page for future auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   resources :students
   resources :branches
   resources :teachers
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/" => "home#index", as: :home
+  get "/register" => "auth#register", as: :register
+  get "/login" => "auth#login", as: :login
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

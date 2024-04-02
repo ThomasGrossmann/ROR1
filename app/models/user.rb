800a908003f8d3247eac1124fd67a1c2746113c8
Thomas Grossmann <thomas.grossmann@eduvaud.ch>
2024-04-02 20:54:57 +0200

    Fix relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :grades
-  has_and_belongs_to_many :school_classes
+  has_and_belongs_to_many :school_classes, join_table: :school_classes_users, class_name: 'SchoolClass', foreign_key: 'school_class_id', association_foreign_key: 'user_id'
   has_and_belongs_to_many :branches
 
   def teacher?
